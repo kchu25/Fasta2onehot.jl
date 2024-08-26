319fda95885fcf207f6fcca5d5ad6c8d2f98dece
Documenter.jl <documenter@juliadocs.github.io>
2024-08-26 18:53:28 +0000

    build based on bc4e9bc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Fasta2onehot","category":"page"},{"location":"#Fasta2onehot","page":"Home","title":"Fasta2onehot","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Fasta2onehot.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Fasta2onehot]","category":"page"}]
+}
