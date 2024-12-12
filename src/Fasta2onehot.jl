e6a60582f16883f41496355526ef7c402ad6db6e
kchu25 <skchu@wustl.edu>
2024-12-12 15:16:11 -0500

    update

--- src/Fasta2onehot.jl
+++ src/Fasta2onehot.jl
@@ -6,6 +6,7 @@ include("dummy.jl")
 include("makedata.jl")
 
 export obtain_training_and_test_set, 
+       onehot_data,
        which_dataset,
        crosslink_mat_num_rows,
        fasta_get_meta_data,
